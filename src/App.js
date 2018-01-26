b5dd3eec1976ac5850ae864f97855a83526026f4
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-01-26 11:31:09 -0700

    start linking shelves

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Imgs/logo.png'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='app'>
+        <div className='nav'>
+          <div className='nav-contents'>
+            {/* <img src='../Imgs/logo.png'/> */}
+            <span className='shelfie'>SHELFIE</span>
+          </div>
+        </div>
+        <div className='shelves'>
+          <div className='shelf'>Shelf A </div>
+          <div className='shelf'>Shelf B </div>
+          <div className='shelf'>Shelf C </div>
+          <div className='shelf'>Shelf D </div>
+        </div>
+       
+        
       </div>
     );
   }
 }
 
-export default App;
