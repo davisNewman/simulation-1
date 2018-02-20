49132c21b2d6c7d6a29cd5540daabb7272cc73d4
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-02-20 12:23:29 -0700

    working on nav

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import './components/Shelves/Shelves.css';
-import image from './imgs/logo.png'
 import routes from './routes'
-import { Link } from 'react-router-dom'
+import './components/Nav/Nav.css'
 
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <div className='nav'>
-          <div className='nav-contents'>
-          {/* 54G & 54E*/ }
-            <img className = 'image' src={image} alt ='logo'/> 
-            {/* 54H */}
-            <Link to='/'><span className='shelfie'>SHELFIE</span></Link>
-          </div>
-        </div>
-        <div className='shelves'>
-          {routes}
-        </div>
+      <div className = 'shelves'>
+       {routes}
       </div>
     );
   }
 }
 
+  
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,79 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import './Nav.css'
+import {link, Link} from 'react-router-dom'
+import '../Nav/Nav.css'
+import image from '../../imgs/logo.png'
 
-export default function NavBar(props){
-    console.log(props.nav,props.binNum)
-    switch(props.nav){
-        case 'A': 
-        return (
-            <div className = 'shelfNav'>
-                <Link to = '/'><button> Shelfie </button></Link>
-                <h1 className = 'shelf'> Shelf A </h1>
-            </div>
-        )  
+export default function Nav (props){
+    console.log(props.url)
+    switch(props.url){
+        case 'A':
+            return(
+                <div>
+                    <Link to='/'><button> SHELFIE </button></Link>
+                    <h1>SHELF A</h1>
+                </div>
+            )
+            break;
+        case 'B':
+            return(
+                 <div >
+                    <Link to='/'><button> SHELFIE </button></Link>
+                    <h1>SHELF B</h1>
+                </div>
+            )
+            break;
+        case 'C':
+            return(
+                 <div>
+                    <Link to='/'><button> SHELFIE </button></Link>
+                    <h1>SHELF C</h1>
+                </div>
+            )
+            break;
+        case 'D':
+            return(
+                 <div>
+                    <Link to='/'><button > SHELFIE </button></Link>
+                    <h1>shelf D</h1>
+                </div>
+            )
+            break;
+        case '/':
+            return(
+                <div className = 'app'>
+                 <div className = 'nav'>
+                    <div className = 'nav-contents'>
+                      {/* 54G & 54E*/ }
+                        <img className = 'image' src = {image} alt = 'logo'/>
+                        {/* 54H */}
+                        <Link to = '/'><span className = 'shelfie'> SHELFIE </span></Link>
+                    </div>
+                </div>
+                </div>
+            )
+            break;
+        case 'bin':
+            return(
+                 <div className = 'nav'>
+
+                </div>
+            )
         break;
+        default: 
+        return <h1>`Hey you've got an error this should not be displayed`</h1>
     }
+    
 }
+
+
+// <div className='nav'>
+// <div className='nav-contents'>
+
+//   <img className = 'image' src={image} alt ='logo'/> 
+
+//   <Link to='/'><span className='shelfie'>SHELFIE</span></Link>
+  
+// </div>
+// </div>
+// <div className='shelves'>
+// </div>
