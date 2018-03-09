124089f83ce0e09e1a058e69ec17931dd0812ef3
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-03-09 16:19:01 -0700

    item css working

--- src/components/Nav/Nav-bin.js
+++ src/components/Nav/Nav-bin.js
@@ -5,6 +5,8 @@ import image from '../../imgs/logo.png'
 
 export default function NavBin (props){
     let param = props.param
+    let param2 = props.param[0]
+    let param3 = props.param[1]
     switch(props.url){
         
         case `/bin/${param}`:
@@ -14,13 +16,13 @@ export default function NavBin (props){
                         <Link to ='/'><img className = 'shelf-image' src = {image} alt = 'logo'/></Link>
                     </div>
                     <div className = 'shelf-title-box'>
-                        <span className = 'shelf-title'> Shelf A </span>
+                        <Link to = {`/bins/${param2}`}><span className = 'shelf-title'> Shelf {param2} </span></Link>
                     </div> 
                     <div className = 'shelf-bin-box'>
-                        <span className = 'shelf-bin-title'> Bin 1</span>
+                        <span className = 'shelf-bin-title'> Bin {param3} </span>
                     </div>
                 </div>
-            )
+        )
         case `/bin/${param}`:
             return(
                 <div className = 'nav-shelf'>
@@ -28,35 +30,41 @@ export default function NavBin (props){
                         <Link to ='/'><img className = 'shelf-image' src = {image} alt = 'logo'/></Link>
                     </div>
                     <div className = 'shelf-title-box'>
-                        <span className = 'shelf-title'> Shelf B </span>
+                        <Link to = {`/bins/${param2}`}><span className = 'shelf-title'> Shelf {param2} </span></Link>
                     </div> 
                     <div className = 'shelf-bin-box'>
-                        <span className = 'shelf-bin-title'> Bin 1</span>
+                        <span className = 'shelf-bin-title'> Bin {param3} </span>
                     </div>
                 </div>
-            )
+        )
         case `/bin/${param}`:
             return(
                 <div className = 'nav-shelf'>
-                <div className = 'logo-holder'>
-                    <Link to ='/'><img className = 'shelf-image' src = {image} alt = 'logo'/></Link>
+                    <div className = 'logo-holder'>
+                        <Link to ='/'><img className = 'shelf-image' src = {image} alt = 'logo'/></Link>
+                    </div>
+                    <div className = 'shelf-title-box'>
+                        <Link to = {`/bins/${param2}`}><span className = 'shelf-title'> Shelf {param2} </span></Link>
+                    </div> 
+                    <div className = 'shelf-bin-box'>
+                        <span className = 'shelf-bin-title'> Bin {param3} </span>
+                    </div>
                 </div>
-                <div className = 'shelf-title-box'>
-                    <span className = 'shelf-title'> Shelf C </span>
-                </div> 
-            </div>
-            )
+        )
         case `/bin/${param}`:
             return(
                 <div className = 'nav-shelf'>
-                <div className = 'logo-holder'>
-                    <Link to ='/'><img className = 'shelf-image' src = {image} alt = 'logo'/></Link>
+                    <div className = 'logo-holder'>
+                        <Link to ='/'><img className = 'shelf-image' src = {image} alt = 'logo'/></Link>
+                    </div>
+                    <div className = 'shelf-title-box'>
+                        <Link to = {`/bins/${param2}`}><span className = 'shelf-title'> Shelf {param2} </span></Link>
+                    </div> 
+                    <div className = 'shelf-bin-box'>
+                        <span className = 'shelf-bin-title'> Bin {param3} </span>
+                    </div>
                 </div>
-                <div className = 'shelf-title-box'>
-                    <span className = 'shelf-title'> Shelf D </span>
-                </div> 
-                </div>
-            )
+        )
         case '/':
             return(
                     <div className = 'nav'>
